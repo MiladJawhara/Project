1e75fc7f66ae37e15438b45b40267d425dc7992b
Milad Jawhara <milad.jawhara@gmail.com>
2020-03-24 02:11:24 +0200

    add routs for admin bla bla bal

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,11 +1,12 @@
 function importPage(path) {
     return () =>
-        import ( /* webpackChunkName: '' */ '../pages/' + path).then(
+        import(/* webpackChunkName: '' */ '../pages/' + path).then(
             m => m.default || m
         )
 }
 
-export default [{
+export default [
+    {
         path: '/login',
         name: 'login',
         component: importPage('auth/Login')
@@ -14,6 +15,14 @@ export default [{
         path: '/register',
         name: 'register',
         component: importPage('auth/Register')
+    },
+    {
+        path: '/admin',
+        children: [
+            {
+                path: '/settings',
+                component: importPage('admin/settings')
+            }
+        ]
     }
-
 ]
