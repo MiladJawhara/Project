f30e284926fb13e7b84d75bb2024519678da24bc
YoussefSaleem <53344261+YoussefSaleem@users.noreply.github.com>
2020-03-26 17:15:03 +0200

    Update settings.vue
    زدت شوية شغلات

--- resources/js/pages/admin/settings.vue
+++ resources/js/pages/admin/settings.vue
@@ -2,15 +2,15 @@
     <v-container>
         <v-row justify="center">
             <v-col :cols="!isMobile ? 8 : 12">
-                <v-card>
-                    <v-card-title class="bg-primary">General Settings</v-card-title>
+                <v-card >
+                    <v-card-title class="bg-primary" prepend-icon="mdi-tune" dense>General Settings</v-card-title>
              <v-card-text>
                         <v-form>
                             <v-container class="pl-0 pr-0">
                                 <v-row>
                                     <v-col>
                                         <v-select
-                                            prepend-icon="mdi-account-cog-outline"
+                                            prepend-icon="mdi-account-clock"
                                             label="Year"
                                             dense
                                             :items="yearsOfStudy"
@@ -24,7 +24,7 @@
                                 <v-row>
                                     <v-col>
                                         <v-select
-                                            prepend-icon="mdi-account-cog-outline"
+                                            prepend-icon="mdi-bank"
                                             label="Dept"
                                             dense
                                             :items="departments"
@@ -37,7 +37,7 @@
                                 <v-row>
                                     <v-col>
                                         <v-select
-                                            prepend-icon="mdi-account-cog-outline"
+                                            prepend-icon="mdi-account-group"
                                             label="#students per group"
                                             dense
                                             :items="num"
@@ -46,20 +46,43 @@
                                         </v-select>
                                     </v-col>
                                 </v-row>
-                          
+                         
+                           <v-form>
                                 <v-row>
                                     <v-col>
                                         <v-text-field
+                                            id="newDI"
                                             type="text"
                                             label="New Dept"
                                             title="Add New Dept"
-                                            prepend-icon="mdi-id-card"
+                                            prepend-icon="mdi-bank-plus"
                                             v-model="form.new_dept"
                                         ></v-text-field>
-                                    </v-col>
-                                </v-row>
-    </v-container>
-   </v-form>        
+                                    
+                              
+                                        <v-btn color="blue" id="addD" @click="addDept">add dept</v-btn>
+                                        </v-col>
+                                  </v-row>
+                           </v-form>
+                                                      <v-form>
+                                <v-row>
+                                    <v-col>
+                                        <v-text-field
+                                            id="newYI"
+                                            type="text"
+                                            label="New Year"
+                                            title="Add New Year"
+                                            prepend-icon="mdi-bank-plus"
+                                            v-model="form.new_year"
+                                        ></v-text-field>
+                                    
+                              
+                                        <v-btn color="blue" id="addY" @click="addYear">add year</v-btn>
+                                        </v-col>
+                                  </v-row>
+                           </v-form>
+    </v-container> 
+      </v-form>    
   </v-card-text>
   </v-card>
   </v-col>
@@ -88,6 +111,22 @@ export default {
             ],
             departments: ['none', 1, 2, 3, 4],
             num: [1, 2, 3, 4, 5, 6, 7, 8]
+        }
+    },
+    methods:{
+        addDept()
+        {
+            var newD=this.departments;
+            newD.splice(newD.length, 1, document.getElementById('newDI').value);
+            thi.departments=newD;
+
+        },
+                addYear()
+        {
+            var newY=this.yearsOfStudy;
+            newY.splice(newY.length, 1, document.getElementById('newYI').value);
+            thi.yearsOfStudy=newY;
+
         }
     },
     computed: {
