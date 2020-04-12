b4710799692c461d6990b7aa635ba5eab668b497
Milad Jawhara <milad.jawhara@gmail.com>
2020-04-12 16:14:31 +0300

    -build

---
+++ public/9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{27:function(e,t,n){"use strict";n.r(t);var r={middleware:"auth"},a=n(3),i=Object(a.a)(r,(function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("router-view")],1)}),[],!1,null,"52f6839a",null);t.default=i.exports}}]);
