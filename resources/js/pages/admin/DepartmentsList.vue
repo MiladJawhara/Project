a5dc94caf08fb7a8944377af6ca150d577e5e754
Milad Jawhara <milad.jawhara@gmail.com>
2020-04-15 00:17:52 +0300

    -Implement Edite Dialog in Departments

--- resources/js/pages/admin/DepartmentsList.vue
+++ resources/js/pages/admin/DepartmentsList.vue
@@ -12,6 +12,8 @@
                     @deleteItem="deleteDepartment"
                     deleteConfirmMsg="Are you sure you want to delete this Departmente? any thing is depending on this will be delete too!!!!"
                 >
+                    <!-- New Department Dialog -->
+
                     <template v-slot:newItemDialog="{ close }">
                         <v-card-title class="primary text-white">
                             Create New Department
@@ -56,6 +58,57 @@
                             >
                         </v-card-actions>
                     </template>
+
+                    <!-- New Department Dialog -->
+
+                    <!-- Edite Department Dialog -->
+
+                    <template v-slot:editDialog="{ item, close }">
+                        <template v-if="item">
+                            <v-card-title class="primary text-white">
+                                Edite Department
+                            </v-card-title>
+
+                            <v-card-text>
+                                <v-form>
+                                    <v-container>
+                                        <v-row>
+                                            <v-col cols="12" md="6">
+                                                <v-text-field
+                                                    label="Title"
+                                                    type="text"
+                                                    v-model="item.title"
+                                                ></v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" md="6">
+                                                <v-select
+                                                    :items="
+                                                        getListOf(
+                                                            'f_name',
+                                                            'supervisors'
+                                                        )
+                                                    "
+                                                    v-model="item.manager"
+                                                    label="Manager"
+                                                >
+                                                </v-select>
+                                            </v-col>
+                                        </v-row>
+                                    </v-container>
+                                </v-form>
+                            </v-card-text>
+
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn
+                                    color="blue"
+                                    text
+                                    @click="updateDepartment(item, close)"
+                                    >Save</v-btn
+                                >
+                            </v-card-actions>
+                        </template>
+                    </template>
                 </data-list>
             </v-col>
         </v-row>
@@ -63,6 +116,7 @@
 </template>
 
 <script>
+import Form from 'vform'
 import DataList from '../../components/gloabal/DataList'
 import { mapActions, mapGetters } from 'vuex'
 export default {
@@ -77,10 +131,15 @@ export default {
         DataList
     },
     methods: {
-        ...mapActions('data', ['request', 'addNewItemTo', 'deleteItemBy']),
+        ...mapActions('data', [
+            'request',
+            'addNewItemTo',
+            'deleteItemBy',
+            'updateItemBy'
+        ]),
         async createNewDepartment(close) {
             close()
-            const { data } = await axios.post('/api/departments', {
+            const { data } = await new Form({
                 title: this.newDepartment.title,
                 dept_manager_id: this.getBy(
                     'id',
@@ -88,10 +147,30 @@ export default {
                     'f_name',
                     this.newDepartment.super
                 )
-            })
+            }).post(`/api/departments`)
 
+            this.newDepartment = {}
             this.addNewItemTo({
                 what: 'departments',
+                item: data
+            })
+        },
+        async updateDepartment(item, close) {
+            close()
+            const { data } = await new Form({
+                title: item.title,
+                dept_manager_id: this.getBy(
+                    'id',
+                    'supervisors',
+                    'f_name',
+                    item.manager
+                )
+            }).put(`/api/departments/${item.id}`)
+
+            this.updateItemBy({
+                from: 'departments',
+                by: 'id',
+                value: item.id,
                 item: data
             })
         },
